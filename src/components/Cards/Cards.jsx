02b432d5dabd8c4135f590958dbe0ab0bf21e7ab
caren1 <czarnocki.w@wp.pl>
2020-09-03 14:22:37 +0200

    initialized material-ui, constructed cards component

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
+import { Card, CardContent, Typography, Grid } from '@material-ui/core'
+import styles from './Cards.module.css'
 
-const Cards = () => {
+const Cards = (props) => {
+
     return (
-        <div>
-            <h1>Cards</h1>
+        <div className={styles.container}>
+            <Grid container spacing={3} justify="center">
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Infected</Typography>
+                        <Typography color="textSecondary" gutterBottom variant="h5">REAL DATA</Typography>
+                        <Typography color="textSecondary">DATE</Typography>  
+                        <Typography variant="body2">Number of active cases of COVID-19</Typography>  
+                    </CardContent>
+                </Grid>
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Recovered</Typography>
+                        <Typography color="textSecondary" gutterBottom variant="h5">REAL DATA</Typography>
+                        <Typography color="textSecondary">DATE</Typography>  
+                        <Typography variant="body2">Number of recovered from COVID-19</Typography>  
+                    </CardContent>
+                </Grid>
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Deaths</Typography>
+                        <Typography color="textSecondary" gutterBottom variant="h5">REAL DATA</Typography>
+                        <Typography color="textSecondary">DATE</Typography>  
+                        <Typography variant="body2">Number of death cases of COVID-19</Typography>  
+                    </CardContent>
+                </Grid>
+            </Grid>
+            
         </div>
     )
 }
