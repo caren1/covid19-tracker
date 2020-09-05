4f071ad59762f7024c2bfd2c35b74649b17eb53c
caren1 <czarnocki.w@wp.pl>
2020-09-05 13:28:28 +0200

    Updates

---
+++ precache-manifest.d9c6c0d5d09835d7f1a716cc274a2f4b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a40e24e1f367264b38502a6f6eea4e9",
+    "url": "/covid19-tracker/index.html"
+  },
+  {
+    "revision": "20779b95c9e18a600d77",
+    "url": "/covid19-tracker/static/css/main.6b4ccad6.chunk.css"
+  },
+  {
+    "revision": "2d7c8c77ec44e2abfde2",
+    "url": "/covid19-tracker/static/js/2.44417a1d.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/covid19-tracker/static/js/2.44417a1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20779b95c9e18a600d77",
+    "url": "/covid19-tracker/static/js/main.4282a9d7.chunk.js"
+  },
+  {
+    "revision": "6b444aef87d753a0bf04",
+    "url": "/covid19-tracker/static/js/runtime-main.815270a9.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19-tracker/static/media/image.d7265326.png"
+  }
+]);
